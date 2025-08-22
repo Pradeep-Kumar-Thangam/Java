6bcc7b0d73b5acc7cab58e8a39a23fe01d2afdb1
Pradeep-Kumar-Thangam <pradeepkumarthangam@gmail.com>
2025-08-22 13:31:00 +0530

    Assignment_1

--- Assignment_1/Q5.java
+++ Assignment_1/Q5.java
@@ -4,13 +4,13 @@ public class Q5 {
     public static void main(String[] args){
         int a = 15, b = 12, c = 18;
         if(a>b && a>c){
-            System.out.print(a);
+            System.out.print("The largest value is : " + a);
         }
         else if(b>c){
-            System.out.print(b);
+            System.out.print("The largest value is : " + b);
         }
         else{
-            System.out.print(c);
+            System.out.print("The largest value is : " + c);
         }
     }
 }
